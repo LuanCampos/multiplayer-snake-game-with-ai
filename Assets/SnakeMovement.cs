bfc935b4cdf70ac424d2627b27e14bb99241426d
LuanCampos <43451938+LuanCampos@users.noreply.github.com>
2021-01-29 15:50:14 -0300

    Snake movement update

--- Assets/SnakeMovement.cs
+++ Assets/SnakeMovement.cs
@@ -9,68 +9,25 @@ public class SnakeMovement : MonoBehaviour
 	private bool isAI = true;
 	
 	private int direction = 0;
+	private int velocity = 15;
 	private int nextMove = 0;
-	private int counter = 60;
+	private int counter = 0;
+	
+	private List<Transform> snake = new List<Transform>();
 	
     void Start()
     {
-		FindOutController();
+		GetControllerType();
+		GetSnakeDots();
+		ResetCounter();
     }
 
     void FixedUpdate()
     {
-		if (counter <= 0)
-		{
-			Debug.Log("Move");
-			
-			if (isAI)
-			{
-				nextMove = aiController.GetNextMove();
-				aiController.HasMoved();
-			}
-			else
-			{
-				nextMove = playerController.GetNextMove();
-				playerController.HasMoved();
-			}
-			
-			if (nextMove == 2)
-			{
-				direction -= 1;
-			}
-			
-			else if (nextMove == 1)
-			{
-				direction += 1;
-			}
-			
-			switch (direction)
-			{
-				case 3:
-					print ("Move RIGHT");
-					break;
-				case 2:
-					print ("Move DOWN");
-					break;
-				case 1:
-					print ("Move LEFT");
-					break;
-				default:
-					print ("Move UP");
-					break;
-			}
-			
-			counter = 60;
-		}
-		
-		else
-		{
-			counter --;
-		}
-
+		CountFrameAndMove();
     }
 	
-	private void FindOutController()
+	private void GetControllerType()
 	{
 		aiController = GetComponent<AIController>();
 		
@@ -79,6 +36,94 @@ public class SnakeMovement : MonoBehaviour
 			playerController = GetComponent<PlayerController>();
 			isAI = false;
 		}
+	}
+	
+	private void GetSnakeDots()
+	{
+		foreach (Transform child in transform)
+		{
+			snake.Add(child);
+		}
+	}
+	
+	private void CountFrameAndMove()
+	{
+		if (counter <= 0)
+		{
+			GetDirection();
+			MoveTheSnake();
+			ResetCounter();
+		}
+		
+		else
+		{
+			counter --;
+		}
+	}
+	
+	private void GetDirection()
+	{
+		if (isAI)
+		{
+			nextMove = aiController.GetNextMove();
+			aiController.HasMoved();
+		}
+		
+		else
+		{
+			nextMove = playerController.GetNextMove();
+			Debug.Log(nextMove);
+			playerController.HasMoved();
+		}
+		
+		if (nextMove == 2)
+		{
+			direction -= 1;
+			
+			if (direction < 0)
+			{
+				direction = 3;
+			}
+		}
+		
+		else if (nextMove == 1)
+		{
+			direction += 1;
+			
+			if (direction > 3)
+			{
+				direction = 0;
+			}
+		}
+	}
+	
+	private void MoveTheSnake()
+	{
+		for (int i = snake.Count -1; i > 0; i--)
+		{
+			snake[i].position = snake[i-1].position;
+		}
+		
+		switch (direction)
+		{
+			case 3:
+				snake[0].position = new Vector3(snake[0].position.x + 1, snake[0].position.y, snake[0].position.z);
+				break;
+			case 2:
+				snake[0].position = new Vector3(snake[0].position.x, snake[0].position.y, snake[0].position.z - 1);
+				break;
+			case 1:
+				snake[0].position = new Vector3(snake[0].position.x - 1, snake[0].position.y, snake[0].position.z);
+				break;
+			default:
+				snake[0].position = new Vector3(snake[0].position.x, snake[0].position.y, snake[0].position.z + 1);
+				break;
+		}
+	}
+	
+	private void ResetCounter()
+	{
+		counter = 20 - velocity;
 	}
 
 }
