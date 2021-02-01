d4be2b6d7db2637d42fcc3ea768bda50e6e16254
LuanCampos <43451938+LuanCampos@users.noreply.github.com>
2021-01-31 21:30:03 -0300

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,70 +18,24 @@ public class GameManager : MonoBehaviour
 	private bool isTheFirstSnake = true;
 	private bool readingLeftKey = false;
 	private bool readingRightKey = false;
+	private int frameCount = 0;	
+	// private int snakeCount = 0;
 	
     void Start()
     {
 		SetVariables();
 		ResetPanels();
 		ShowPanels();
-		
-		leftKeyText.characterLimit = 1;
-		rightKeyText.characterLimit = 1;
-		
-		readingLeftKey = true;
     }
 	
     void Update()
     {
         if (!gameStarted)
 		{
-			if (readingLeftKey)
-			{
-				leftKeyText.ActivateInputField();
-				
-				if (leftKeyText.text.Length > 0)
-				{
-					char c = leftKeyText.text[0];
-					
-					if (char.IsDigit (c) || char.IsLetter (c))
-					{
-						string cString = c + "";
-						leftKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), cString.ToUpper());
-						leftKeyText.DeactivateInputField();
-						rightKeyText.ActivateInputField();
-						readingLeftKey = false;
-						readingRightKey = true;
-					}
-					
-					else
-					{
-						leftKeyText.text = "";
-					}
-				}
-			}
-			
-			if (readingRightKey)
-			{
-				rightKeyText.ActivateInputField();
-				
-				if (rightKeyText.text.Length > 0)
-				{
-					char c = rightKeyText.text[0];
-					
-					if (char.IsDigit (c) || char.IsLetter (c))
-					{
-						string cString = c + "";
-						rightKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), cString.ToUpper());
-						rightKeyText.DeactivateInputField();
-						readingRightKey = false;
-					}
-					
-					else
-					{
-						rightKeyText.text = "";
-					}
-				}
-			}			
+			StartGame();
+			ReadKeys();
+			ChangeTheSnake();
+			SetTheSnakeUp();
 		}
     }
 	
@@ -106,6 +60,8 @@ public class GameManager : MonoBehaviour
 		pressEnterPanel = GameObject.Find("Press Enter Panel");
 		leftKeyText = GameObject.Find("InputField Left").GetComponent<InputField>();
 		rightKeyText = GameObject.Find("InputField Right").GetComponent<InputField>();
+		leftKeyText.characterLimit = 1;
+		rightKeyText.characterLimit = 1;
 	}
 	
 	private void ResetPanels()
@@ -136,6 +92,103 @@ public class GameManager : MonoBehaviour
 		else
 		{
 			choseKeysPanel.GetComponent<RectTransform>().localPosition += new Vector3(160, 0, 0);
+		}
+		
+		readingLeftKey = true;
+	}
+	
+	private void StartGame()
+	{
+		if (!isTheFirstSnake && !readingRightKey)
+		{
+			if (Input.GetKeyDown(KeyCode.Return)
+			{
+				// START THE GAME
+			}
+		}
+	}
+	
+	private void ReadKeys()
+	{
+		if (readingLeftKey)
+		{
+			leftKeyText.ActivateInputField();
+			
+			if (leftKeyText.text.Length > 0)
+			{
+				char c = leftKeyText.text[0];
+				
+				if (char.IsDigit (c) || char.IsLetter (c))
+				{
+					string cString = c + "";
+					leftKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), cString.ToUpper());
+					leftKeyText.DeactivateInputField();
+					rightKeyText.ActivateInputField();
+					readingLeftKey = false;
+					readingRightKey = true;
+					// SPAWN THE SNAKE
+				}
+				
+				else
+				{
+					leftKeyText.text = "";
+				}
+			}
+		}
+			
+		if (readingRightKey)
+		{
+			rightKeyText.ActivateInputField();
+			
+			if (rightKeyText.text.Length > 0)
+			{
+				char c = rightKeyText.text[0];
+				
+				if (char.IsDigit (c) || char.IsLetter (c))
+				{
+					string cString = c + "";
+					rightKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), cString.ToUpper());
+					rightKeyText.DeactivateInputField();
+					readingRightKey = false;
+				}
+				
+				else
+				{
+					rightKeyText.text = "";
+				}
+			}
+		}
+	}
+	
+	private void ChangeTheSnake()
+	{
+		if (!readingLeftKey && !readingRightKey)
+		{
+			if (Input.GetKey(rightKey))
+			{
+				if (frameCount > 50)
+				{
+					// TROCAR DE SNAKE
+					frameCount = 0;
+				}
+				frameCount ++;
+			}
+		}
+	}
+	
+	private void SetTheSnakeUp()
+	{
+		if (!readingLeftKey && !readingRightKey)
+		{
+			if (!Input.GetKey(rightKey))
+			{
+				// SETAR A PORRA TODA
+				frameCount = 0;
+				isTheFirstSnake = false;
+				isOddNumber = !isOddNumber;
+				ResetPanels();
+				ShowPanels();
+			}
 		}
 	}
 	
